86a0daec43461c01f981bb8cc0c97484c1e713b2
Yozzaxia1311 <yozzaxia@gmail.com>
2019-03-14 09:52:01 -0400

    indent fix main.lua

--- main.lua
+++ main.lua
@@ -240,38 +240,38 @@ function love.draw()
 end
 
 function love.run()
-	if love.math then
-		love.math.setRandomSeed(os.time())
-	end
-	if love.load then love.load(arg) end
-	if love.timer then love.timer.step() end
-	local dt = 0
-	while true do
-		if love.event then
-			love.event.pump()
-			for name, a,b,c,d,e,f in love.event.poll() do
-				if name == "quit" then
-					if not love.quit or not love.quit() then
-						return a
-					end
-				end
-				love.handlers[name](a,b,c,d,e,f)
-			end
-		end
-		if love.timer then
-			love.timer.step()
-			dt = love.timer.getDelta()
-		end
+  if love.math then
+    love.math.setRandomSeed(os.time())
+  end
+  if love.load then love.load(arg) end
+  if love.timer then love.timer.step() end
+  local dt = 0
+  while true do
+    if love.event then
+      love.event.pump()
+      for name, a,b,c,d,e,f in love.event.poll() do
+        if name == "quit" then
+          if not love.quit or not love.quit() then
+            return a
+          end
+        end
+        love.handlers[name](a,b,c,d,e,f)
+      end
+    end
+    if love.timer then
+      love.timer.step()
+      dt = love.timer.getDelta()
+    end
     local before_update = love.timer.getTime()
-		if love.update then love.update(dt) end
-		if love.graphics and love.graphics.isActive() then
-			love.graphics.clear(love.graphics.getBackgroundColor())
-			love.graphics.origin()
-			if love.draw then love.draw() end
+    if love.update then love.update(dt) end
+    if love.graphics and love.graphics.isActive() then
+      love.graphics.clear(love.graphics.getBackgroundColor())
+      love.graphics.origin()
+      if love.draw then love.draw() end
       love.graphics.present()
-		end
-		local delta = love.timer.getTime() - before_update
+    end
+    local delta = love.timer.getTime() - before_update
     if delta < framerate then love.timer.sleep(framerate - delta) end
     resized = false
-	end
+  end
 end
