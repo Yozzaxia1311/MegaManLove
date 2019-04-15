98c331f5106471d3147fbb13d8dc16e8679e4df6
Yozzaxia1311 <yozzaxia@gmail.com>
2019-04-15 15:52:15 -0400

    remove this

--- demogame/assets/tilesets/demo_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="demo_tileset" tilewidth="16" tileheight="16" tilecount="12" columns="3">
- <image source="demo_tileset.png" width="48" height="64"/>
-</tileset>
--- demogame/assets/tilesets/menu_tiled.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="menu_tiled" tilewidth="8" tileheight="8" tilecount="240" columns="16">
- <image source="menu_tiled.png" width="128" height="120"/>
-</tileset>
