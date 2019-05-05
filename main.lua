4f2fa17674e9080956c2c84df445086378fcd95f
Yozzaxia1311 <yozzaxia@gmail.com>
2019-05-05 15:31:31 -0400

    update love.run

--- main.lua
+++ main.lua
@@ -277,38 +277,35 @@ function love.quit()
 end
 
 function love.run()
-  if love.math then
-    love.math.setRandomSeed(os.time())
-  end
-  if love.load then love.load(arg) end
+  if love.load then love.load(love.arg.parseGameArguments(arg), arg) end
   if love.timer then love.timer.step() end
   local dt = 0
-  while true do
-    if love.event then
-      love.event.pump()
-      for name, a,b,c,d,e,f in love.event.poll() do
-        if name == "quit" then
-          if not love.quit or not love.quit() then
-            return a
+  return function()
+      if love.event then
+        love.event.pump()
+        for name, a,b,c,d,e,f in love.event.poll() do
+          if name == "quit" then
+            if not love.quit or not love.quit() then
+              return a or 0
+            end
           end
+          love.handlers[name](a,b,c,d,e,f)
         end
-        love.handlers[name](a,b,c,d,e,f)
       end
+      if love.timer then
+        love.timer.step()
+        dt = love.timer.getDelta()
+      end
+      local before_update = love.timer.getTime()
+      if love.update then love.update(dt) end
+      if love.graphics and love.graphics.isActive() then
+        love.graphics.origin()
+        love.graphics.clear(love.graphics.getBackgroundColor())
+        if love.draw then love.draw() end
+        love.graphics.present()
+      end
+      local delta = love.timer.getTime() - before_update
+      if delta < framerate then love.timer.sleep(framerate - delta) end
+      resized = false
     end
-    if love.timer then
-      love.timer.step()
-      dt = love.timer.getDelta()
-    end
-    local before_update = love.timer.getTime()
-    if love.update then love.update(dt) end
-    if love.graphics and love.graphics.isActive() then
-      love.graphics.clear(love.graphics.getBackgroundColor())
-      love.graphics.origin()
-      if love.draw then love.draw() end
-      love.graphics.present()
-    end
-    local delta = love.timer.getTime() - before_update
-    if delta < framerate then love.timer.sleep(framerate - delta) end
-    resized = false
-  end
 end
