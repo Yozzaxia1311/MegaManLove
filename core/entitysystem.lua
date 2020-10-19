504bd2a1871c4fcfe5fc5c2bc9b3d7e9c9ba215f
Yozzaxia1311 <yozzaxia@gmail.com>
2020-10-19 14:35:29 -0400

    Update entitysystem.lua

--- core/entitysystem.lua
+++ core/entitysystem.lua
@@ -629,8 +629,8 @@ function basicEntity:setCircleCollision(r)
   self.collisionShape = {}
   self.collisionShape.type = 2
   self.collisionShape.r = r
-  self.collisionShape.w = r
-  self.collisionShape.h = r
+  self.collisionShape.w = r * 2
+  self.collisionShape.h = r * 2
 end
 
 function basicEntity:collision(e, x, y, notme)
@@ -692,8 +692,7 @@ function basicEntity:collisionTable(t, x, y, notme, func)
   local result = {}
   if not t then return result end
   for k=1, #t do
-    local v = t[k]
-    if self:collision(v, x, y, notme) and (func == nil and true or func(v)) then
+    if self:collision(t[k], x, y, notme) and (func == nil or func(t[k])) then
       result[#result+1] = v
     end
   end
@@ -704,8 +703,7 @@ function basicEntity:collisionNumber(t, x, y, notme, func)
   local result = 0
   if not t then return result end
   for k=1, #t do
-    local v = t[k]
-    if self:collision(v, x, y, notme) and (func == nil and true or func(v)) then
+    if self:collision(t[k], x, y, notme) and (func == nil or func(t[k])) then
       result = result + 1
     end
   end
