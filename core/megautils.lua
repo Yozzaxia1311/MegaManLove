b25b507110e925a67c745bc0be3c87fa9c041640
Yozzaxia1311 <yozzaxia@gmail.com>
2020-12-03 16:33:48 -0500

    revert this

--- core/megautils.lua
+++ core/megautils.lua
@@ -937,15 +937,3 @@ function megautils.rectStencil(x, y, w, h)
     love.graphics.rectangle("fill", _stenx, _steny, _stenw, _stenh)
   end
 end
-
-function megautils.getClassString(c)
-  if type(c) ~= "Entity" then
-    return type(c)
-  end
-  
-  for k, v in pairs(_G)
-    if v == c then
-      return k
-    end
-  end
-end
