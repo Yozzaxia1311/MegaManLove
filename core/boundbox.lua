bc3d63cd54fc25aca107d2048ac17f32e857d70f
Yozzaxia1311 <yozzaxia@gmail.com>
2021-01-01 12:04:47 -0500

    better approach to img col

--- core/boundbox.lua
+++ core/boundbox.lua
@@ -35,9 +35,8 @@ end
 function imageOverlapsRect(x, y, w, h, data, x2, y2, w2, h2)
   if rectOverlapsRect(x2, y2, w2, h2, x, y, w, h) then
     local neww, newh = w-1, h-1
-    
-    for xi=math.clamp(math.round(x2-x), 0, neww), math.clamp(math.round(x2-x)+w2, 0, neww) do
-      for yi=math.clamp(math.round(y2-y), 0, newh), math.clamp(math.round(y2-y)+h2, 0, newh) do
+    for xi=math.clamp(math.floor(x2-x), 0, neww), math.clamp(math.ceil(x2-x)+w2, 0, neww) do
+      for yi=math.clamp(math.floor(y2-y), 0, newh), math.clamp(math.ceil(y2-y)+h2, 0, newh) do
         local _, _, _, a = data:getPixel(xi, yi)
         if a > 0 and rectOverlapsRect(x + xi, y + yi, 1, 1, x2, y2, w2, h2) then
           return true
@@ -52,8 +51,8 @@ function imageOverlapsCircle(x, y, w, h, data, x2, y2, r2)
   if circleOverlapsRect(x2, y2, r2, x, y, w, h) then
     local neww, newh = w-1, h-1
     
-    for xi=math.clamp(math.round(x2-x)-r2, 0, neww), math.clamp(math.round(x2-x)+r2, 0, neww) do
-      for yi=math.clamp(math.round(y2-y)-r2, 0, newh), math.clamp(math.round(y2-y)+r2, 0, newh) do
+    for xi=math.clamp(math.floor(x2-x)-r2, 0, neww), math.clamp(math.ceil(x2-x)+r2, 0, neww) do
+      for yi=math.clamp(math.floor(y2-y)-r2, 0, newh), math.clamp(math.ceil(y2-y)+r2, 0, newh) do
         local _, _, _, a = data:getPixel(xi, yi)
         if a > 0 and circleOverlapsRect(x2, y2, r2, x + xi, y + yi, 1, 1) then
           return true
@@ -69,10 +68,10 @@ function imageOverlapsImage(x, y, w, h, data, x2, y2, w2, h2, data2)
     local neww, newh = w-1, h-1
     local neww2, newh2 = w2-1, h2-1
     
-    for xi=math.clamp(math.round(x2-x), 0, neww), math.clamp(math.round(x2-x)+w2, 0, neww) do
-      for yi=math.clamp(math.round(y2-y), 0, newh), math.clamp(math.round(y2-y)+h2, 0, newh) do
-        for xi2=math.clamp(math.round(x-x2), 0, neww2), math.clamp(math.round(x-x2)+w, 0, neww2) do
-          for yi2=math.clamp(math.round(y-y2), 0, newh2), math.clamp(math.round(y-y2)+h, 0, newh2) do
+    for xi=math.clamp(math.floor(x2-x), 0, neww), math.clamp(math.ceil(x2-x)+w2, 0, neww) do
+      for yi=math.clamp(math.floor(y2-y), 0, newh), math.clamp(math.ceil(y2-y)+h2, 0, newh) do
+        for xi2=math.clamp(math.floor(x-x2), 0, neww2), math.clamp(math.ceil(x-x2)+w, 0, neww2) do
+          for yi2=math.clamp(math.floor(y-y2), 0, newh2), math.clamp(math.ceil(y-y2)+h, 0, newh2) do
             local _, _, _, a = data:getPixel(xi, yi)
             local _, _, _, a2 = data2:getPixel(xi2, yi2)
             if a > 0 and a2 > 0 and rectOverlapsRect(x + xi, y + yi, 1, 1, x2 + xi2, y2 + yi2, 1, 1) then
