c602b1d00a3945e4c1a9e9cdd2a6d55e7b66f3e3
Yozzaxia1311 <yozzaxia@gmail.com>
2021-02-01 03:13:41 -0500

    oh boy

--- core/tween.lua
+++ core/tween.lua
@@ -28,6 +28,8 @@ tween = {
   ]]
 }
 
+tween
+
 -- easing
 
 -- Adapted from https://github.com/EmmanuelOga/easing. See LICENSE.txt for credits.
@@ -40,171 +42,171 @@ tween = {
 local sin, cos, pi, sqrt, abs, asin = math.sin, math.cos, math.pi, math.sqrt, math.abs, math.asin
 
 -- linear
-local function linear(t, b, c, d) return c * t / d + b end
+function tween.linear(t, b, c, d) return c * t / d + b end
 
 -- quad
-local function inQuad(t, b, c, d) return c * ((t / d) ^ 2) + b end
-local function outQuad(t, b, c, d)
+function tween.inQuad(t, b, c, d) return c * ((t / d) ^ 2) + b end
+function tween.outQuad(t, b, c, d)
   t = t / d
   return -c * t * (t - 2) + b
 end
-local function inOutQuad(t, b, c, d)
+function tween.inOutQuad(t, b, c, d)
   t = t / d * 2
   if t < 1 then return c / 2 * (t ^ 2) + b end
   return -c / 2 * ((t - 1) * (t - 3) - 1) + b
 end
-local function outInQuad(t, b, c, d)
-  if t < d / 2 then return outQuad(t * 2, b, c / 2, d) end
-  return inQuad((t * 2) - d, b + c / 2, c / 2, d)
+function tween.outInQuad(t, b, c, d)
+  if t < d / 2 then return tween.outQuad(t * 2, b, c / 2, d) end
+  return tween.inQuad((t * 2) - d, b + c / 2, c / 2, d)
 end
 
 -- cubic
-local function inCubic (t, b, c, d) return c * ((t / d) ^ 3) + b end
-local function outCubic(t, b, c, d) return c * (((t / d - 1) ^ 3) + 1) + b end
-local function inOutCubic(t, b, c, d)
+function tween.inCubic (t, b, c, d) return c * ((t / d) ^ 3) + b end
+function tween.outCubic(t, b, c, d) return c * (((t / d - 1) ^ 3) + 1) + b end
+function tween.inOutCubic(t, b, c, d)
   t = t / d * 2
   if t < 1 then return c / 2 * t * t * t + b end
   t = t - 2
   return c / 2 * (t * t * t + 2) + b
 end
-local function outInCubic(t, b, c, d)
-  if t < d / 2 then return outCubic(t * 2, b, c / 2, d) end
-  return inCubic((t * 2) - d, b + c / 2, c / 2, d)
+function tween.outInCubic(t, b, c, d)
+  if t < d / 2 then return tween.outCubic(t * 2, b, c / 2, d) end
+  return tween.inCubic((t * 2) - d, b + c / 2, c / 2, d)
 end
 
 -- quart
-local function inQuart(t, b, c, d) return c * ((t / d) ^ 4) + b end
-local function outQuart(t, b, c, d) return -c * (((t / d - 1) ^ 4) - 1) + b end
-local function inOutQuart(t, b, c, d)
+function tween.inQuart(t, b, c, d) return c * ((t / d) ^ 4) + b end
+function tween.outQuart(t, b, c, d) return -c * (((t / d - 1) ^ 4) - 1) + b end
+function tween.inOutQuart(t, b, c, d)
   t = t / d * 2
   if t < 1 then return c / 2 * (t ^ 4) + b end
   return -c / 2 * (((t - 2) ^ 4) - 2) + b
 end
-local function outInQuart(t, b, c, d)
+function tween.outInQuart(t, b, c, d)
   if t < d / 2 then return outQuart(t * 2, b, c / 2, d) end
   return inQuart((t * 2) - d, b + c / 2, c / 2, d)
 end
 
 -- quint
-local function inQuint(t, b, c, d) return c * ((t / d) ^ 5) + b end
-local function outQuint(t, b, c, d) return c * (((t / d - 1) ^ 5) + 1) + b end
-local function inOutQuint(t, b, c, d)
+function tween.inQuint(t, b, c, d) return c * ((t / d) ^ 5) + b end
+function tween.outQuint(t, b, c, d) return c * (((t / d - 1) ^ 5) + 1) + b end
+function tween.inOutQuint(t, b, c, d)
   t = t / d * 2
   if t < 1 then return c / 2 * (t ^ 5) + b end
   return c / 2 * (((t - 2) ^ 5) + 2) + b
 end
-local function outInQuint(t, b, c, d)
-  if t < d / 2 then return outQuint(t * 2, b, c / 2, d) end
-  return inQuint((t * 2) - d, b + c / 2, c / 2, d)
+function tween.outInQuint(t, b, c, d)
+  if t < d / 2 then return tween.outQuint(t * 2, b, c / 2, d) end
+  return tween.inQuint((t * 2) - d, b + c / 2, c / 2, d)
 end
 
 -- sine
-local function inSine(t, b, c, d) return -c * cos(t / d * (pi / 2)) + c + b end
-local function outSine(t, b, c, d) return c * sin(t / d * (pi / 2)) + b end
-local function inOutSine(t, b, c, d) return -c / 2 * (cos(pi * t / d) - 1) + b end
-local function outInSine(t, b, c, d)
-  if t < d / 2 then return outSine(t * 2, b, c / 2, d) end
-  return inSine((t * 2) -d, b + c / 2, c / 2, d)
+function tween.inSine(t, b, c, d) return -c * cos(t / d * (pi / 2)) + c + b end
+function tween.outSine(t, b, c, d) return c * math.sin(t / d * (pi / 2)) + b end
+function tween.inOutSine(t, b, c, d) return -c / 2 * (cos(pi * t / d) - 1) + b end
+function tween.outInSine(t, b, c, d)
+  if t < d / 2 then return tween.outSine(t * 2, b, c / 2, d) end
+  return tween.inSine((t * 2) -d, b + c / 2, c / 2, d)
 end
 
 -- expo
-local function inExpo(t, b, c, d)
+function tween.inExpo(t, b, c, d)
   if t == 0 then return b end
   return c * (2 ^ (10 * (t / d - 1))) + b - c * 0.001
 end
-local function outExpo(t, b, c, d)
+function tween.outExpo(t, b, c, d)
   if t == d then return b + c end
   return c * 1.001 * (-(2 ^ (-10 * t / d) + 1)) + b
 end
-local function inOutExpo(t, b, c, d)
+function tween.inOutExpo(t, b, c, d)
   if t == 0 then return b end
   if t == d then return b + c end
   t = t / d * 2
   if t < 1 then return c / 2 * (2 ^ (10 * (t - 1))) + b - c * 0.0005 end
   return c / 2 * 1.0005 * (-(2 ^ (-10 * (t - 1))) + 2) + b
 end
-local function outInExpo(t, b, c, d)
-  if t < d / 2 then return outExpo(t * 2, b, c / 2, d) end
-  return inExpo((t * 2) - d, b + c / 2, c / 2, d)
+function tween.outInExpo(t, b, c, d)
+  if t < d / 2 then return tween.outExpo(t * 2, b, c / 2, d) end
+  return tween.inExpo((t * 2) - d, b + c / 2, c / 2, d)
 end
 
 -- circ
-local function inCirc(t, b, c, d) return(-c * (sqrt(1 - ((t / d) ^ 2)) - 1) + b) end
-local function outCirc(t, b, c, d)  return(c * sqrt(1 - ((t / d - 1) ^ 2)) + b) end
-local function inOutCirc(t, b, c, d)
+function tween.inCirc(t, b, c, d) return(-c * (sqrt(1 - ((t / d) ^ 2)) - 1) + b) end
+function tween.outCirc(t, b, c, d)  return(c * sqrt(1 - ((t / d - 1) ^ 2)) + b) end
+function tween.inOutCirc(t, b, c, d)
   t = t / d * 2
   if t < 1 then return -c / 2 * (sqrt(1 - t * t) - 1) + b end
   t = t - 2
   return c / 2 * (sqrt(1 - t * t) + 1) + b
 end
-local function outInCirc(t, b, c, d)
-  if t < d / 2 then return outCirc(t * 2, b, c / 2, d) end
-  return inCirc((t * 2) - d, b + c / 2, c / 2, d)
+function tween.outInCirc(t, b, c, d)
+  if t < d / 2 then return tween.outCirc(t * 2, b, c / 2, d) end
+  return tween.inCirc((t * 2) - d, b + c / 2, c / 2, d)
 end
 
 -- elastic
-local function calculatePAS(p,a,c,d)
+function tween.calculatePAS(p,a,c,d)
   p, a = p or d * 0.3, a or 0
   if a < abs(c) then return p, c, p / 4 end -- p, a, s
   return p, a, p / (2 * pi) * asin(c/a) -- p,a,s
 end
-local function inElastic(t, b, c, d, a, p)
+function tween.inElastic(t, b, c, d, a, p)
   local s
   if t == 0 then return b end
   t = t / d
   if t == 1  then return b + c end
   p,a,s = calculatePAS(p,a,c,d)
   t = t - 1
-  return -(a * (2 ^ (10 * t)) * sin((t * d - s) * (2 * pi) / p)) + b
+  return -(a * (2 ^ (10 * t)) * math.sin((t * d - s) * (2 * pi) / p)) + b
 end
-local function outElastic(t, b, c, d, a, p)
+function tween.outElastic(t, b, c, d, a, p)
   local s
   if t == 0 then return b end
   t = t / d
   if t == 1 then return b + c end
-  p,a,s = calculatePAS(p,a,c,d)
-  return a * (2 ^ (-10 * t)) * sin((t * d - s) * (2 * pi) / p) + c + b
+  p,a,s = tween.tween.calculatePAS(p,a,c,d)
+  return a * (2 ^ (-10 * t)) * math.sin((t * d - s) * (2 * pi) / p) + c + b
 end
-local function inOutElastic(t, b, c, d, a, p)
+function tween.inOutElastic(t, b, c, d, a, p)
   local s
   if t == 0 then return b end
   t = t / d * 2
   if t == 2 then return b + c end
-  p,a,s = calculatePAS(p,a,c,d)
+  p,a,s = tween.calculatePAS(p,a,c,d)
   t = t - 1
-  if t < 0 then return -0.5 * (a * (2 ^ (10 * t)) * sin((t * d - s) * (2 * pi) / p)) + b end
-  return a * (2 ^ (-10 * t)) * sin((t * d - s) * (2 * pi) / p ) * 0.5 + c + b
+  if t < 0 then return -0.5 * (a * (2 ^ (10 * t)) * math.sin((t * d - s) * (2 * pi) / p)) + b end
+  return a * (2 ^ (-10 * t)) * math.sin((t * d - s) * (2 * pi) / p ) * 0.5 + c + b
 end
-local function outInElastic(t, b, c, d, a, p)
-  if t < d / 2 then return outElastic(t * 2, b, c / 2, d, a, p) end
-  return inElastic((t * 2) - d, b + c / 2, c / 2, d, a, p)
+function tween.outInElastic(t, b, c, d, a, p)
+  if t < d / 2 then return tween.outElastic(t * 2, b, c / 2, d, a, p) end
+  return tween.inElastic((t * 2) - d, b + c / 2, c / 2, d, a, p)
 end
 
 -- back
-local function inBack(t, b, c, d, s)
+function tween.inBack(t, b, c, d, s)
   s = s or 1.70158
   t = t / d
   return c * t * t * ((s + 1) * t - s) + b
 end
-local function outBack(t, b, c, d, s)
+function tween.outBack(t, b, c, d, s)
   s = s or 1.70158
   t = t / d - 1
   return c * (t * t * ((s + 1) * t + s) + 1) + b
 end
-local function inOutBack(t, b, c, d, s)
+function tween.inOutBack(t, b, c, d, s)
   s = (s or 1.70158) * 1.525
   t = t / d * 2
   if t < 1 then return c / 2 * (t * t * ((s + 1) * t - s)) + b end
   t = t - 2
   return c / 2 * (t * t * ((s + 1) * t + s) + 2) + b
 end
-local function outInBack(t, b, c, d, s)
-  if t < d / 2 then return outBack(t * 2, b, c / 2, d, s) end
-  return inBack((t * 2) - d, b + c / 2, c / 2, d, s)
+function tween.outInBack(t, b, c, d, s)
+  if t < d / 2 then return tween.outBack(t * 2, b, c / 2, d, s) end
+  return tween.inBack((t * 2) - d, b + c / 2, c / 2, d, s)
 end
 
 -- bounce
-local function outBounce(t, b, c, d)
+function tween.outBounce(t, b, c, d)
   t = t / d
   if t < 1 / 2.75 then return c * (7.5625 * t * t) + b end
   if t < 2 / 2.75 then
@@ -217,35 +219,35 @@ local function outBounce(t, b, c, d)
   t = t - (2.625 / 2.75)
   return c * (7.5625 * t * t + 0.984375) + b
 end
-local function inBounce(t, b, c, d) return c - outBounce(d - t, 0, c, d) + b end
-local function inOutBounce(t, b, c, d)
-  if t < d / 2 then return inBounce(t * 2, 0, c, d) * 0.5 + b end
-  return outBounce(t * 2 - d, 0, c, d) * 0.5 + c * .5 + b
+function tween.inBounce(t, b, c, d) return c - tween.outBounce(d - t, 0, c, d) + b end
+function tween.inOutBounce(t, b, c, d)
+  if t < d / 2 then return tween.inBounce(t * 2, 0, c, d) * 0.5 + b end
+  return tween.outBounce(t * 2 - d, 0, c, d) * 0.5 + c * .5 + b
 end
-local function outInBounce(t, b, c, d)
-  if t < d / 2 then return outBounce(t * 2, b, c / 2, d) end
-  return inBounce((t * 2) - d, b + c / 2, c / 2, d)
+function tween.outInBounce(t, b, c, d)
+  if t < d / 2 then return tween.outBounce(t * 2, b, c / 2, d) end
+  return tween.inBounce((t * 2) - d, b + c / 2, c / 2, d)
 end
 
 tween.easing = {
-  linear    = linear,
-  inQuad    = inQuad,    outQuad    = outQuad,    inOutQuad    = inOutQuad,    outInQuad    = outInQuad,
-  inCubic   = inCubic,   outCubic   = outCubic,   inOutCubic   = inOutCubic,   outInCubic   = outInCubic,
-  inQuart   = inQuart,   outQuart   = outQuart,   inOutQuart   = inOutQuart,   outInQuart   = outInQuart,
-  inQuint   = inQuint,   outQuint   = outQuint,   inOutQuint   = inOutQuint,   outInQuint   = outInQuint,
-  inSine    = inSine,    outSine    = outSine,    inOutSine    = inOutSine,    outInSine    = outInSine,
-  inExpo    = inExpo,    outExpo    = outExpo,    inOutExpo    = inOutExpo,    outInExpo    = outInExpo,
-  inCirc    = inCirc,    outCirc    = outCirc,    inOutCirc    = inOutCirc,    outInCirc    = outInCirc,
-  inElastic = inElastic, outElastic = outElastic, inOutElastic = inOutElastic, outInElastic = outInElastic,
-  inBack    = inBack,    outBack    = outBack,    inOutBack    = inOutBack,    outInBack    = outInBack,
-  inBounce  = inBounce,  outBounce  = outBounce,  inOutBounce  = inOutBounce,  outInBounce  = outInBounce
+  linear    = tween.linear,
+  inQuad    = tween.inQuad,    outQuad    = tween.outQuad,    inOutQuad    = tween.inOutQuad,    outInQuad    = tween.outInQuad,
+  inCubic   = tween.inCubic,   outCubic   = tween.outCubic,   inOutCubic   = tween.inOutCubic,   outInCubic   = tween.outInCubic,
+  inQuart   = tween.inQuart,   outQuart   = tween.outQuart,   inOutQuart   = tween.inOutQuart,   outInQuart   = tween.outInQuart,
+  inQuint   = tween.inQuint,   outQuint   = tween.outQuint,   inOutQuint   = tween.inOutQuint,   outInQuint   = tween.outInQuint,
+  inSine    = tween.inSine,    outSine    = tween.outSine,    inOutSine    = tween.inOutSine,    outInSine    = tween.outInSine,
+  inExpo    = tween.inExpo,    outExpo    = tween.outExpo,    inOutExpo    = tween.inOutExpo,    outInExpo    = tween.outInExpo,
+  inCirc    = tween.inCirc,    outCirc    = tween.outCirc,    inOutCirc    = tween.inOutCirc,    outInCirc    = tween.outInCirc,
+  inElastic = tween.inElastic, outElastic = tween.outElastic, inOutElastic = tween.inOutElastic, outInElastic = tween.outInElastic,
+  inBack    = tween.inBack,    outBack    = tween.outBack,    inOutBack    = tween.inOutBack,    outInBack    = tween.outInBack,
+  inBounce  = tween.inBounce,  outBounce  = tween.outBounce,  inOutBounce  = tween.inOutBounce,  outInBounce  = tween.outInBounce
 }
 
 
 
 -- private stuff
 
-local function copyTables(destination, keysTable, valuesTable)
+function tween.copyTables(destination, keysTable, valuesTable)
   valuesTable = valuesTable or keysTable
   local mt = getmetatable(keysTable)
   if mt and getmetatable(destination) == nil then
@@ -261,7 +263,7 @@ local function copyTables(destination, keysTable, valuesTable)
   return destination
 end
 
-local function checkSubjectAndTargetRecursively(subject, target, path)
+function tween.checkSubjectAndTargetRecursively(subject, target, path)
   path = path or {}
   local targetType, newPath
   for k,targetValue in pairs(target) do
@@ -277,7 +279,7 @@ local function checkSubjectAndTargetRecursively(subject, target, path)
   end
 end
 
-local function checkNewParams(duration, subject, target, easing)
+function tween.checkNewParams(duration, subject, target, easing)
   assert(type(duration) == 'number' and duration > 0, "duration must be a positive number. Was " .. tostring(duration))
   local tsubject = type(subject)
   assert(tsubject == 'table' or tsubject == 'userdata', "subject must be a table or userdata. Was " .. tostring(subject))
@@ -286,7 +288,7 @@ local function checkNewParams(duration, subject, target, easing)
   checkSubjectAndTargetRecursively(subject, target)
 end
 
-local function getEasingFunction(easing)
+function tween.getEasingFunction(easing)
   easing = easing or "linear"
   if type(easing) == 'string' then
     local name = easing
@@ -298,7 +300,7 @@ local function getEasingFunction(easing)
   return easing
 end
 
-local function performEasingOnSubject(subject, target, initial, clock, duration, easing)
+function tween.performEasingOnSubject(subject, target, initial, clock, duration, easing)
   local t,b,c,d
   for k,v in pairs(target) do
     if type(v) == 'table' then
